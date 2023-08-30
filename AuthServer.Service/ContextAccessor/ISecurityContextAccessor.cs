e6d8e8b253356adff605506fc712e2650f1f2232
ChessWizard <chesswizard2002@gmail.com>
2023-08-30 11:42:41 +0300

    comments deleted

--- AuthServer.Service/ContextAccessor/ISecurityContextAccessor.cs
+++ AuthServer.Service/ContextAccessor/ISecurityContextAccessor.cs
@@ -2,10 +2,8 @@
 
 namespace AuthServer.Data.ContextAccessor
 {
-    // Http isteği geldikten sonra authentication bilgilerini yakalamamıza yarayacak interface
     public interface ISecurityContextAccessor
     {
-        // Veriler yalnızca get edilsin, içeride değiştirilemesinler
         public Guid TokenId { get; }
 
         public Guid UserId { get; }
